472584e728072f51da59df8eb6a04a5a48647108
shaik-md-musayyeb <smd52952@gmail.com>
2020-03-21 23:22:38 -0700

    modified

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,25 @@
 package controller;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws ClassNotFoundException, IOException, SQLException {
 		
 	// Fill your code
+
+//		ConnectionManager cm = new ConnectionManager();
+		Connection c = null;
+		c= ConnectionManager.getConnection();
+		if(c!= null) {
+			System.out.println("Connection established");
+		}
+		else
+			System.out.println("Incorrect username / password");
+		
 		
 	}
 }
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,9 +1,30 @@
 package utility;
 
-
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
+	public static Connection getConnection() throws ClassNotFoundException, IOException, SQLException {
+		Properties p = loadPropertiesFile();
+		Class.forName(p.getProperty("driver"));
+		Connection conn = null;
+		conn = DriverManager.getConnection(p.getProperty("url"),p.getProperty("username"),p.getProperty("password"));
+		
+		return conn;
+		
+	}
 	
+	public static Properties loadPropertiesFile() throws IOException {
+		 Properties prop = new Properties();
+		 InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		 prop.load(in); 
+		 in.close();
+		 return prop;
+		
+	}
 	
 }
